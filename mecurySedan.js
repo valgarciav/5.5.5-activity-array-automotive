37035e98851f435ce9a4d089694e6fc29e485f59
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-04-02 13:04:00 -0400

    first commit
    submit

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,8 +1,58 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+const VehicleModule = require("./vehicle").vehicle
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
-import {}
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("the engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+    let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+    myCar.start()
+    myCar.loadPassenger(5)
+    myCar.stop()
+    myCar.checkService()
+    
+    console.log(myCar)
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+    
